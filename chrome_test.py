cbebf735e24b2049423031f8aefddda576ed1d45
Ubuntu <ubuntu@ip-172-31-9-100.eu-west-1.compute.internal>
2016-11-10 10:37:55 +0000

    Wow, I wasn't expecting Chrome to actually work

--- chrome_test.py
+++ chrome_test.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 
 driver = webdriver.Remote(
-  command_executor = "http://52.210.77.132:4444/wd/hub",
+  command_executor = "http://localhost:4444/wd/hub",
   desired_capabilities = {
-    "browserName": "firefox",
+    "browserName": "chrome",
     #"platform": "LINUX",
   }
 )
---
+++ firefox_test.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+driver = webdriver.Remote(
+  command_executor = "http://localhost:4444/wd/hub",
+  desired_capabilities = {
+    "browserName": "firefox",
+    #"platform": "LINUX",
+  }
+)
+
+try:
+  driver.implicitly_wait(30)
+  driver.get("http://www.python.org")
+  print(driver.title)
+  assert "Python" in driver.title
+finally:
+  driver.quit()
